1f661abf68bcf13727f24bffb4b53f9938da1df2
liulijun <1937249556@qq.com>
2019-12-30 17:59:23 +0800

    提交时：vue-webpack

--- src/main.js
+++ src/main.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-=======
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
->>>>>>> init
