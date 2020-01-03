194cbc16637a520114b07b10da05a91577f91880
liulijun <1937249556@qq.com>
2020-01-03 17:59:51 +0800

    提交： vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports{
-    
+// vue.config.js
+module.exports = {
+    productionSourceMap: false,      //是否生成调试文件.map
+    filenameHashing:false,           //默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。然而，这也要求 index 的 HTML 是被 Vue CLI 自动生成的。如果你无法使用 Vue CLI 生成的 index HTML，你可以通过将这个选项设为 false 来关闭文件名哈希。
 }
