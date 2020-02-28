c5da65ee36ea52985a4350e14d2d9dc2cf9728eb
liulijun <1937249556@qq.com>
2020-02-28 08:53:20 +0800

    提交：vue.config.js

--- test.js
+++ test.js
@@ -1,30 +1,28 @@
-var _ = require('lodash');
+var _ = require('lodash')
 
+var array = [1]
+var other = _.concat(array, 2, [3], [[4]])
 
-
-var array = [1];
-var other = _.concat(array, 2, [3], [[4]]);
- 
-console.log(other);
+console.log(other)
 // => [1, 2, 3, [4]]
- 
-console.log(array);
 
-var data = [1,2,3,4,5];
+console.log(array)
 
-console.log(_.chunk(data,2)[1]);
+var data = [1, 2, 3, 4, 5]
 
-//json5支持注释：//与/**/
-let json_1={
-    "path": "http//www.baidu.com",/*地址*/
-    "master":{
-         "rootport":9999,//端口
-         "webport":9998,
-         "log":"./logs/masterlog.log"//log文件路径
-    }
- 
+console.log(_.chunk(data, 2)[1])
+
+// json5支持注释：//与/**/
+let json_1 = {
+  'path': 'http//www.baidu.com', /* 地址 */
+  'master': {
+    'rootport': 9999, // 端口
+    'webport': 9998,
+    'log': './logs/masterlog.log'// log文件路径
+  }
+
 }
-console.log(json_1.path);
+console.log(json_1.path)
 
-console.log(process.env.VUE_APP_SECRET);
-console.log(_.join(['Another', 'module', 'loaded!'], ' '));
+console.log(process.env.VUE_APP_SECRET)
+console.log(_.join(['Another', 'module', 'loaded!'], ' '))
